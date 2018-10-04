6a06c588f06d6e2961f43f9895e0097902d27032
Shuo Wang <gken90@gmail.com>
2018-10-04 22:58:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#__________________________________________________________________
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]                   move style[0] into name
+#   },
+
+
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, info|   #style = oo & functional
+    info.each do |language, type|
+      new_hash = new_hash.merge!(info)
+        # new_hash = {:style => style}
+      # new_hash.merge!(style)
+    end
+  end
+  puts new_hash
 end
